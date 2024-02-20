e6fccbe7f5c1edbca316dcdbf93180f5277c0d36
Eduardo Carvalho <eduardo.carvalho@trafegus.com.br>
2024-02-20 19:55:03 -0300

    Criado campos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import data_handler as dh
-import util 
+import util
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
@@ -26,18 +26,42 @@ if(data_analyses_on):
 
 st.header('Preditor de Diabetes')
 
-col1, col2, col3 = st.columns(3)
-
+col1, col2 = st.columns([1,2])
 with col1:
-    classes = ["1st", "2nd", "3rd"]
-    p_class = st.selectbox('Ticket class', classes)
+    vezesEngravidou = st.number_input('Número de vezes que já engravidou', min_value=0, max_value=20, value=2, step=1)
 
 with col2:
-    classes = ['Male', 'Female']
-    sex = st.selectbox('Sex', classes)
+    glicose = st.number_input('Concentração de glicose pós 2h no teste oral de tolerância à glicose', min_value=0, max_value=200, value=85, step=1)
 
-with col3:
-    age = st.number_input('Age', min_value=0, max_value=100, value=0, step=1)
+col1, col2 = st.columns(2)
+with col1:
+    pressao = st.number_input('Pressão arterial diastólica (mm Hg)', min_value=0, max_value=122, value=100, step=1)
+
+with col2:
+    espessuraPele = st.number_input('Espessura da dobra cutânea do tríceps (mm)', min_value=0.00, max_value=99.00, value=30.50, step=0.01)
+
+col1, col2= st.columns(2)
+
+with col1:
+    insulina = st.number_input('Insulina sérica de 2 horas (mu U/ml)', min_value=0, max_value=900, value=100, step=1)
+
+with col2:
+    imc = st.number_input('Índice de massa corporal (peso em kg/(altura em m)^2)', min_value=0, max_value=100, value=50, step=1)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    funcaoPedigree = st.number_input('Função de pedigree do diabetes', min_value=0.00, max_value=3.00, value=1.50, step=0.01)
+
+with col2:
+    idade = st.number_input('Idade', min_value=0, max_value=120, value=30, step=1)
+
+# with col2:
+#     classes = ['Male', 'Female']
+#     sex = st.selectbox('Sex', classes)
+
+# with col3:
+#     age = st.number_input('Age', min_value=0, max_value=100, value=0, step=1)
 
 col1, col2, col3 = st.columns([2,2,1])
 
@@ -99,7 +123,7 @@ if(submit or 'survived' in st.session_state):
             st.subheader('Passageiro Não sobreviveu ')
             if 'survived' in st.session_state:
                 st.snow()
-            
+
     st.session_state['survived'] = survived
 
     if passageiro and 'survived' in st.session_state:
@@ -118,20 +142,20 @@ if(submit or 'survived' in st.session_state):
             if wrong_prediction:
                 message += ", iremos usar esses dados para melhorar as predições"
             message += "."
-            
+
             # adiciona no dict do passageiro se a predição está correta ou não
             if correct_prediction:
                 passageiro['CorrectPrediction'] = True
             elif wrong_prediction:
                 passageiro['CorrectPrediction'] = False
-                
+
             # adiciona no dict do passageiro se ele sobreviveu ou não
             passageiro['Survived'] = st.session_state['survived']
-            
+
             # escreve a mensagem na tela
             st.write(message)
             print(message)
-            
+
             # salva a predição no JSON para cálculo das métricas de avaliação do sistema
             dh.save_prediction(passageiro)
 
@@ -140,7 +164,7 @@ if(submit or 'survived' in st.session_state):
         col1, col2, col3 = st.columns(3)
         with col2:
             new_test = st.button('Iniciar Nova Análise')
-            
+
             # se o usuário pressionar no botão e já existe um passageiro, remove ele do cache
             if new_test and 'survived' in st.session_state:
                 del st.session_state['survived']
@@ -148,31 +172,30 @@ if(submit or 'survived' in st.session_state):
         accuracy_predictions_on = st.toggle('Exibir acurácia')
 
         if accuracy_predictions_on:
-            
+
             predictions = dh.get_all_predictions()
-            
+
             num_total_predictions = len(predictions)
-            
+
             accuracy_hist = [0]
-            
+
             correct_predictions = 0
-            
+
             for index, passageiro in enumerate(predictions):
                 total = index + 1
                 if passageiro['CorrectPrediction'] == True:
                     correct_predictions += 1
-                    
+
                 temp_accuracy = correct_predictions / total if total else 0
-                
+
                 accuracy_hist.append(round(temp_accuracy, 2))
 
             accuracy = correct_predictions / num_total_predictions if num_total_predictions else 0
 
             st.metric(label='Acurácia', value=round(accuracy, 2))
             # TODO: usar o attr delta do st.metric para exibir a diferença na variação da acurácia
-            
+
             # exibe o histórico da acurácia
             st.subheader("Histórico de acurácia")
             st.line_chart(accuracy_hist)
 
-    
